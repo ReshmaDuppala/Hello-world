df04fd0682f55c2e6ad3a9d85fccffd43dfb6133
ReshmaDuppala <51434420+ReshmaDuppala@users.noreply.github.com>
2020-04-29 12:42:09 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AuthorsComponent } from './authors/authors.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { PanelComponent } from './panel/panel.component';
+import { LikeComponent } from './like/like.component';
+import { ZippyComponent } from './zippy/zippy.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular app';
+tweet = {
+  body: '...',
+  likesCount: 10,
+  isActive: true
+}
+
+
+}
---
+++ app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { PanelComponent } from './panel/panel.component';
+import { LikeComponent } from './like/like.component';
+import { ZippyComponent } from './zippy/zippy.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthorsComponent,
+    FavoriteComponent,
+    PanelComponent,
+    LikeComponent,
+    ZippyComponent,
+    NewCourseFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [AuthorsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
